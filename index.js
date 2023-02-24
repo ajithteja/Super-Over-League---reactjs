4780d4aca566fa8a90cb6e4cd670fd828164628b
ajithteja <ajithtejag@gmail.com>
2023-02-24 13:14:39 +0000

    Created basic react project

---
+++ index.js
@@ -0,0 +1,20 @@
+const element = (
+  //   Write your code here.
+  <div className="bg-container">
+    <div className="cart-container">
+      <h1 className="heading">Super Over League</h1>
+      <div>
+        <img
+          className="img"
+          src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+        />
+        <img
+          className="img"
+          src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+        />
+      </div>
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
